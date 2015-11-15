8e2104470d8118e043a11fd58ecded86e4c172c6
Bastian Ruppert <info@bastian-ruppert.de>
2015-11-15 19:20:53 +0100

    mpd_ctrl.py: first steps with classes
    Signed-off-by: Bastian Ruppert <info@bastian-ruppert.de>

--- mpd_ctrl.py
+++ mpd_ctrl.py
@@ -1,18 +1,25 @@
 #mpd_ctrl.py control mpd over mpc
-#import time
-#import RPIO as GPIO
 
 class mpcCtrl:
     
+    info = 'this is a mpcCtrl Class' # class variable shared by all instances
+
     def __init__(self, name):
-        self.name = name # instance variable unique to each instance
+        self.position = 0   # instance variable unique to each instance
         self.sender = []    # creates a new empty list
 
     def addSender(self, sender):
         self.sender.append(sender)
 
-    def getName(self):
-        return self.name
+    def getPosition(self):
+        return self.position
+
+    def getSenderAtCurrentPos(self):
+        if len(self.sender)-1 >= self.position:
+            return self.sender[self.position]
+        else:
+            return 0
+        
 
     def printSender(self):
         for string in self.sender:
@@ -35,13 +42,19 @@ sender = [
 print 'Du hast',len(sender),'Sender in der Senderliste'
 
 ctrl = mpcCtrl('derName')
-ctrl.addSender(sender)
+
+print 'SenderAtCurrentPos:',ctrl.getSenderAtCurrentPos()
+
+for s in sender:
+    ctrl.addSender(s)
+#ctrl.addSender(sender[0])
 #ctrl.addSender(sender[1])
 #ctrl.addSender(sender[2])
 #ctrl.addSender(sender[3])
 
-print 'Der Name lautet:',ctrl.getName()
-ctrl.printSender()
+print 'Die momentane Position ist:',ctrl.getPosition()
+#ctrl.printSender()
+print 'SenderAtCurrentPos:',ctrl.getSenderAtCurrentPos()
     
 
 """
