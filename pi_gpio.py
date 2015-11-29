33445217736d8180bcace75aba8764468d43fd1c
Bastian Ruppert <info@bastian-ruppert.de>
2015-11-29 21:59:32 +0100

    pi_gpio.py: timer wieder rausgeschmissen, mit polling alle 100ms weitergemacht
    Signed-off-by: Bastian Ruppert <info@bastian-ruppert.de>

--- pi_gpio.py
+++ pi_gpio.py
@@ -1,6 +1,5 @@
 import time
 import RPi.GPIO as GPIO
-import datetime as dt
 
 def gpio_init():
 
@@ -11,36 +10,33 @@ def gpio_init():
   # Pin 18 (GPIO 24) auf Input setzen
   GPIO.setup(18, GPIO.IN)
 
-  GPIO.add_event_detect(18,GPIO.BOTH) # add rising edge detection on a channel
+  #GPIO.add_event_detect(18,GPIO.BOTH) # add rising edge detection on a channel
 
   # Pin 11 (GPIO 17) auf Output setzen
   GPIO.setup(11, GPIO.OUT)
 
-timeHigh = dt.datetime.now()
-timeLow = dt.datetime.now()
-def dauer_gpio_pressed(IsHigh):#returns dauer in Sekunden
-  global timeHigh
-  global timeLow
+BtnHigh = True
+BtnLowCnt = 0
 
-  if IsHigh:#Messung beendet
-    timeHigh = dt.datetime.now()
-    #print 'dauer_gpio_pressed IsHigh', timeHigh
-    tmpTime = (timeHigh-timeLow)
-    print 'tmpTime: ', tmpTime
-    #print 'die vergangene Zeit: ', (timeHigh-timeLow), "in Microsekunden", (tmpTime.seconds/1000+tmpTime.microsecond)/1e6
-    return (timeHigh.microsecond-timeLow.microsecond)/1e6
-  else: #IsLow
-    timeLow = dt.datetime.now()
-    #print 'dauer_gpio_pressed IsLow', timeLow
-    return 0
+def gpio_dispatcher():#alle 100 ms
+  global BtnHigh
+  global BtnLowCnt
 
-def gpio_dispatcher():
   if GPIO.input(18) == GPIO.HIGH:
-    print 'gpio high'
-    print 'dauer_gpio_pressed: ',dauer_gpio_pressed(1)
+    if not BtnHigh:
+      BtnHigh = True
+      print 'gpio changed to High, time in Seconds = ',(BtnLowCnt*0.1) 
+      BtnLowCnt = 0
+      print 'gpio changed to High, Button released'
   elif GPIO.input(18) == GPIO.LOW:
-    print 'gpio low'
-    dauer_gpio_pressed(0)
+    if BtnHigh:
+      BtnHigh = False
+      print 'gpio changed to Low'
+    if not BtnHigh:
+      BtnLowCnt = BtnLowCnt + 1
+
+  if BtnLowCnt >= 30:
+     print "EXIT Signal"
 
 gpio_init()
 
@@ -49,17 +45,17 @@ counter = 0
 while 1:
   time.sleep(0.1)
 
+  gpio_dispatcher()
+
 #  counter = counter+1
-  if counter >= 10:
+  if counter >= 100:
     break # stops the loop
 
   # LED immer anmachen
   GPIO.output(11, GPIO.HIGH)
 
-  if GPIO.event_detected(18):
-    gpio_dispatcher()
-    print('gpio 18 event_detected')
-    counter = counter+1
+  #print('gpio 18 event_detected')
+  counter = counter+1
 
   # GPIO lesen
 #  if GPIO.input(18) == GPIO.HIGH:
@@ -76,3 +72,25 @@ print 'ende aus!'
 GPIO.cleanup(18)
 GPIO.cleanup(11)
 
+
+
+"""
+import datetime as dt
+
+timeHigh = dt.datetime.now()
+timeLow = dt.datetime.now()
+def dauer_gpio_pressed(IsHigh):#returns dauer in Sekunden
+  global timeHigh
+  global timeLow
+
+  if IsHigh:#Messung beendet
+    timeHigh = dt.datetime.now()
+    #print 'dauer_gpio_pressed IsHigh', timeHigh
+    tmpTime = (timeHigh-timeLow)
+    print 'tmpTime in Sekunden: ', (tmpTime.seconds + tmpTime.microseconds/1e6)
+    return (tmpTime.seconds + tmpTime.microseconds/1e6)
+  else: #IsLow
+    timeLow = dt.datetime.now()
+    #print 'dauer_gpio_pressed IsLow', timeLow
+    return 0
+"""
